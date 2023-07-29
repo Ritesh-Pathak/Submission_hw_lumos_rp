420b76bacd0f022a8ed6512d94124abde56d2f41
Ritesh-Pathak <138897590+Ritesh-Pathak@users.noreply.github.com>
2023-07-29 13:49:31 +0530

    Add files via upload

--- quote.js
+++ quote.js
@@ -1,20 +1,10 @@
-const express=  require('express');
-const router=express.Router();
-const Quote=require('../models/quote');
-router.get('/',async(req,res)=>{
-    const quotes= await Quote.find();
-    res.status(200).json(quotes);
+const mongoose= require('mongoose');
+const quoteSchema=new mongoose.Schema({
+    author:{
+        type: String,
+    },
+    quote:{
+        type:String,
+    }
 })
-
-router.post('/',async(req,res)=>{
-    const quote= new Quote({
-        author:req.body.author,
-        quote:req.body.quote
-    })
-    try{
-        const newQuote=await quote.save();
-        res.status(201).json(newQuote);
-    }catch(err){res.status(400).json({message:err.message});
-}
-})
-module.exports=router;
+module.exports=mongoose.model('Quote',quoteSchema);
